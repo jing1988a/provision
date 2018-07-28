2555a3916b993e1844cbcfb1eff3da78f7f0f284
jing1988a <jing1988a@gmail.com>
2018-07-28 11:16:12 -0400

    78

---
+++ java/src/Subsets78.java
@@ -0,0 +1,52 @@
+//Given a set of distinct integers, nums, return all possible subsets (the power set).
+//
+//        Note: The solution set must not contain duplicate subsets.
+//
+//        Example:
+//
+//        Input: nums = [1,2,3]
+//        Output:
+//        [
+//        [3],
+//        [1],
+//        [2],
+//        [1,2,3],
+//        [1,3],
+//        [2,3],
+//        [1,2],
+//        []
+//        ]
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        int l=nums.length;
+        dfs(nums , 0 , l , new ArrayList<Integer>() , ans);
+        return ans;
+
+    }
+    private void dfs(int[] nums , int idx , int l , List<Integer> candidate , List<List<Integer>> ans){
+        ans.add(deepCopy(candidate));
+        if(idx==l){
+            return;
+        }
+        for(int i=idx ; i<l;i++){
+            candidate.add(nums[i]);
+            dfs(nums ,i+1 ,l , candidate , ans);
+            candidate.remove(candidate.size()-1);
+        }
+
+
+    }
+    private List<Integer> deepCopy(List<Integer> original){
+        List<Integer> copy=new ArrayList<>();
+        for(Integer i:original){
+            copy.add(i);
+        }
+        return copy;
+    }
+}
