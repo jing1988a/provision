9c1620be80efe55b4be6f54c023a4a23324ec5af
jing1988a <jing1988a@gmail.com>
2018-07-30 14:43:21 -0400

    670

---
+++ java/src/MaximumSwap670.java
@@ -0,0 +1,66 @@
+//
+//Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. Return the maximum valued number you could get.
+//
+//        Example 1:
+//        Input: 2736
+//        Output: 7236
+//        Explanation: Swap the number 2 and the number 7.
+//        Example 2:
+//        Input: 9973
+//        Output: 9973
+//        Explanation: No swap.
+//        Note:
+//        The given number is in the range [0, 108]
+
+
+import java.util.Arrays;
+
+public class MaximumSwap670 {
+    public int maximumSwap(int num) {
+        if(num<10){return num;}
+        int[] nums=intToArray(num);
+        int l=nums.length;
+        int[][] rightMax=new int[l][2];
+        int[] cur={0 , l};
+        for(int i=l-1 ; i>-1 ; i-- ){
+            rightMax[i]= Arrays.copyOf(cur , cur.length);
+            if(rightMax[i][0]<nums[i]){
+                cur[0]=nums[i];
+                cur[1]=i;
+            }
+        }
+        for(int i=0 ; i<l-1 ; i++){
+            if (rightMax[i][0]>nums[i]) {
+                int idx=rightMax[i][1];
+                int temp=nums[idx];
+                nums[idx]=nums[i];
+                nums[i]=temp;
+                return toInt(nums);
+            }
+        }
+        return num;
+    }
+    private int[] intToArray(int num){
+        String nStr=String.valueOf(num);
+        int l =nStr.length();
+        int[] ans=new int[l];
+        for(int i=0; i<l ; i++){
+            ans[i]=Character.getNumericValue(nStr.charAt(i));
+        }
+        return ans;
+
+    }
+    private int toInt(int[] nums){
+        int ans=0;
+        for(int n :nums){
+            ans=ans*10+n;
+        }
+        return ans;
+    }
+
+    static public void main(String[] arg){
+        int nums=9973;
+        MaximumSwap670 test= new MaximumSwap670();
+        test.maximumSwap(nums);
+    }
+}
