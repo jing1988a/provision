83e22ed33d647c103eb08c7558db6b1846002450
jing1988a <jing1988a@gmail.com>
2018-08-01 14:19:58 -0400

    221

--- java/src/MaximalSquare221.java
+++ java/src/MaximalSquare221.java
@@ -20,6 +20,31 @@
 
 public class MaximalSquare221 {
     public int maximalSquare(char[][] matrix) {
+        int n=matrix.length;
+        if(n==0){return 0;}
+        int m=matrix[0].length;
+        if(m==0){return 0 ;}
+        int[][] dp= new int[n][m];
+        int ans=0;
+        for(int i=0 ; i<n ;i++){
+            for(int j=0 ; j <m ; j++ ){
+                if(matrix[i][j]=='1'){
+                    dp[i][j]=1;
+                    ans=1;
+                }
+            }
+        }
+
+        for(int i=1 ; i<n ;i++){
+            for(int j=1 ; j <m ; j++ ){
+                if(matrix[i][j]=='1'){
+                    dp[i][j]=1+Math.min(dp[i-1][j] ,  Math.min(dp[i][j-1] , dp[i-1][j-1]));
+                    ans=Math.max(ans , dp[i][j]);
+                }
+            }
+        }
+        return ans*ans;
+
 
     }
 }
