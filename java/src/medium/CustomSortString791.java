5a9be6d422fcfd53ab44c8326631b49a70ad01d3
jing1988a <jing1988a@gmail.com>
2018-08-05 11:15:11 -0400

    791

---
+++ java/src/medium/CustomSortString791.java
@@ -0,0 +1,52 @@
+package medium;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+//S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+//
+//        S was sorted in some custom order previously. We want to permute the characters of T so that they match the order that S was sorted. More specifically, if x occurs before y in S, then x should occur before y in the returned string.
+//
+//        Return any permutation of T (as a string) that satisfies this property.
+//
+//        Example :
+//        Input:
+//        S = "cba"
+//        T = "abcd"
+//        Output: "cbad"
+//        Explanation:
+//        "a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a".
+//        Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs.
+//
+//
+//        Note:
+//
+//        S has length at most 26, and no character is repeated in S.
+//        T has length at most 200.
+//        S and T consist of lowercase letters only.
+public class CustomSortString791 {
+    public String customSortString(String S, String T) {
+        Map<Character , Integer>  weight=new HashMap<>();
+        for(int i=0 ; i<26 ; i++){
+            weight.put(  (char)( 'a'+i) , 26);
+        }
+        // System.out.println(weight);
+        for(int i=0 ; i<S.length() ; i++){
+            weight.put(S.charAt(i) , i);
+        }
+//        char[] temp= T.toCharArray();    // char[] 的话 下面 那个lambda  没法 work 智障一样。
+        Character temp[] = new Character[T.length()];
+        for (int i = 0; i < T.length(); i++) {
+            temp[i] = T.charAt(i);
+        }
+        Arrays.sort(temp , (x , y)->weight.get(x)-weight.get(y)  );
+        StringBuilder tempAns= new StringBuilder();
+        for(Character c : temp){
+            tempAns.append(c);
+        }
+
+        // return temp.toString();  "[Ljava.lang.Character;@2d98a335"
+        return tempAns.toString();
+    }
+}
