7b95dfe71dc14ffed066b082d893bf521ce171e6
jing1988a <jing1988a@gmail.com>
2018-08-07 18:17:44 -0400

    10

--- java/src/RegularExpressionMatching10.java
+++ java/src/RegularExpressionMatching10.java
@@ -69,7 +69,7 @@ public class RegularExpressionMatching10 {
             for(int i=1 ;i<ls+1 ; i++){
                 for(int j=1 ; j<lp+1 ; j++){
                     if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='.'){
-                        dp[i][j]=dp[i][j-1];
+                        dp[i][j]=dp[i-1][j-1];
                     }else if(p.charAt(j-1)=='*'){
                         dp[i][j]=dp[i][j-2]||   dp[i][j-1]     ||( (s.charAt(i-1)==p.charAt(j-2)  || p.charAt(j-2)=='.') &&  dp[i-1][j]);
                     }
