c3126c0238dacb73ffdf22e903af1e4d85d83a79
jing1988a <jing1988a@gmail.com>
2018-08-08 23:09:35 -0400

    84

--- java/src/LargestRectangleHistogram84.java
+++ java/src/LargestRectangleHistogram84.java
@@ -16,6 +16,10 @@
 //        Output: 10
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class LargestRectangleHistogram84 {
 //    public int largestRectangleArea(int[] heights) {
 //        int l=heights.length;
@@ -38,7 +42,41 @@ public class LargestRectangleHistogram84 {
     // above is O(n^2)
 
     public int largestRectangleArea(int[] heights) {
-        
+        Stack<List<Integer>> stack=new Stack<>();
+        List<Integer> temp0=new ArrayList<>();
+        temp0.add(heights[0]);
+        temp0.add(0);
+        stack.push(temp0);
+        int l=heights.length;
+        int ans=0;
+        for(int i=1 ; i<l ; i++){
+            int curIdx=i;
+            while(!stack.isEmpty() && stack.peek().get(0)>heights[i]){
+                List<Integer> cur=stack.pop();
+                ans=Math.max(ans , (i-cur.get(1))*cur.get(0));
+                curIdx=cur.get(1);
+            }
+            if(stack.isEmpty() || stack.peek().get(0)<heights[i]) {
+                List<Integer> temp = new ArrayList<>();
+                temp.add(heights[i]);
+                temp.add(curIdx);
+                stack.push(temp);
+            }
+
+        }
+        while(!stack.isEmpty()){
+            List<Integer> left=stack.pop();
+
+            ans=Math.max(ans , left.get(0)*(l-left.get(1)));
+        }
+        return ans;
+    }
+
+    static public void main(String[] args){
+        LargestRectangleHistogram84 test=new LargestRectangleHistogram84();
+        int[] h={4,2,0,3,2,5};
+        int ans=test.largestRectangleArea(h);
+        System.out.println(ans);
     }
 
 
