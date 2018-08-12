3685446b4aee5e6b2ea069f13a2e343df267f000
jing1988a <jing1988a@gmail.com>
2018-08-11 22:38:05 -0400

    122

---
+++ java/src/Yanyan.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Yanyan {
+    static public void main(String[] args){
+        int[] newIntArray=new int[4];
+        int[] intArray={1 , 2 , 3 , 4};
+        for (int n:intArray) {
+            System.out.println(n);
+        }
+
+
+    }
+}
