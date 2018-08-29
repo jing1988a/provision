3255934b265cb57c41f1973bcf42819449b37d6d
jing1988a <jing1988a@gmail.com>
2018-08-29 14:50:12 -0400

    255

--- java/src/fb/medium/SplitArraywithEqualSum548.java
+++ java/src/fb/medium/SplitArraywithEqualSum548.java
@@ -42,3 +42,44 @@ public class SplitArraywithEqualSum548 {
         return false;
     }
 }
+
+
+
+
+//解题思路：
+//        利用数组sums记录前n项和
+//
+//        利用字典idxs统计sums元素对应的下标列表
+//
+//        根据sums和idxs枚举满足(0, i - 1) == (i + 1, j - 1)条件的i，j。利用字典jlist记录子数组和对应的j值列表
+//
+//        最后遍历k，枚举jlist中子数组和(k + 1, n - 1)对应的j值，然后判断是否存在 (j + 1, k - 1)  与 (k + 1, n - 1) 相等
+//
+//        Python代码：
+//class Solution(object):
+//        def splitArray(self, nums):
+//        """
+//        :type nums: List[int]
+//        :rtype: bool
+//        """
+//        size = len(nums)
+//        sums = [0] * (size + 1)
+//        for x in range(size):
+//        sums[x + 1] += sums[x] + nums[x]
+//
+//        idxs = collections.defaultdict(list)
+//        for x in range(size):
+//        idxs[sums[x + 1]].append(x)
+//
+//        jlist = collections.defaultdict(list)
+//        for i in range(1, size):
+//        for j in idxs[2 * sums[i] + nums[i]]:
+//        if i < j < size:
+//        jlist[sums[i]].append(j + 1)
+//
+//        for k in range(size - 2, 0, -1):
+//        for j in jlist[sums[size] - sums[k + 1]]:
+//        if j + 1 > k: continue
+//        if sums[k] - sums[j + 1] == sums[size] - sums[k + 1]:
+//        return True
+//        return False
